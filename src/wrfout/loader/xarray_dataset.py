2a6cfd39280576b7390c0314963328ea1d8f7e22
kobatake_yusuke <suu_hppa@outlook.jp>
2025-05-03 03:24:39 +0900

    Add: filenotfound error handling

--- src/wrfout/loader/xarray_dataset.py
+++ src/wrfout/loader/xarray_dataset.py
@@ -1,10 +1,13 @@
-import xarray as xr
+import os
 
+import xarray as xr
 from time_relation.conversion import datetime64s_to_datetimes
 
 
 class WrfoutXarrayDataset:
     def __init__(self, wrfout_path: str) -> None:
+        if not os.path.exists(wrfout_path):
+            raise FileNotFoundError(f"{wrfout_path} does not exist.")
         self.dataset = xr.open_dataset(wrfout_path)
 
     def _set_time_lat_lon_coords(self) -> None:
